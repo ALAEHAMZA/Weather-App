06a60f20584b6185f902ac8686ea622b63371d52
Alae Benhamza <alaebenhamza3@gmail.com>
2022-12-15 21:10:54 +0100

    Update Weather.jsx

--- src/Components/weather/Weather.jsx
+++ src/Components/weather/Weather.jsx
@@ -4,16 +4,22 @@ import React,{useState,useEffect} from 'react';
 import {BsSun} from 'react-icons/bs';
 import {BsCloudy} from 'react-icons/bs';
 import {BsCloudRainHeavy} from "react-icons/bs"
+import {BsCloudSnow} from "react-icons/bs";
 import {CiSearch} from "react-icons/ci" ;
+import {IoThunderstormOutline} from "react-icons/io5";
+import {BsCloudHaze1} from "react-icons/bs";
 
 export default function Weather() {
     const [infos,setInfos]=useState([]);
-    const [location,setLocation]=useState('Tetouan');
+    const [location,setLocation]=useState('tetouan');
     const [input,setInput]=useState('');
 
-    const handleSubmit=()=>{
+    const handleSubmit=(e)=>{
+        e.preventDefault();
         setLocation(input);
     };
+
+
     useEffect(() => {
         const getData = async () => {
             const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=d1195038077f099ac192273d37c20b77`);
@@ -21,12 +27,11 @@ export default function Weather() {
         };
     
         getData().then((infos) => setInfos(infos));
-        }, []);
+        }, [location]);
 
     console.log(infos);
 
-console.log(infos.main)
-
+    
     let d = new Date();
     let date = d.getDate();
     let year = d.getFullYear();
@@ -37,25 +42,67 @@ console.log(infos.main)
         minute:'2-digit'
     });
 
+    let icon = null;
+    let background =null;
+    if (typeof infos.main != "undefined") {
+        switch (infos.weather[0].main) {
+            case "Clouds":
+                icon = <BsCloudy/>
+                background = "clouds"
+                break;
+            case "Clear":
+                icon = <BsSun/> 
+                background = "clair"
+                break; 
+            case "Drizzle":
+                icon = <BsCloudRainHeavy/> 
+                background = "rain"
+                break;            
+            case "Rain":
+                icon = <BsCloudRainHeavy/> 
+                background = "rain"
+                break;
+            case "Haze":
+                icon = <BsCloudHaze1/>
+                background = "haze" 
+                break;
+            case "Thunderstorm":
+                icon = <IoThunderstormOutline/> 
+                background = "haze"
+                break;
+            case "Snow":
+                icon = <BsCloudSnow/>
+                background = "snow"
+                break;
+            default:
+                icon = <BsCloudy/>
+                background = "clouds"
+                break;
+        }
+    }else{
+        return "LOADING..."
+    }
 
     return(
-        <div className="general">
+        <div className={background}>
+            {infos.main !== undefined ? (
+                <>
             <div className='left'>
                 <h2 className='title'>the.weather</h2>
                 <div className="container">
-                    <div className="temp">{infos.main.temp}</div>
+                    <div className="temp">{Math.floor(infos.main.temp-273.15)}º</div>
                     <div className="placeDate">
                         <div className="place">{infos.name}</div>
                         <div className="date">{time} - {day}, {date} {month}, {year}</div>
                     </div>
                     <div className="mainLogo">
-                        <div className="logo"><BsSun/></div>
-                        <div className="main">{infos.weather[0]["main"]}</div>
+                        <div className="logo">{icon}</div>
+                        <div className="main">{infos.weather[0].main}</div>
                     </div>
                 </div>
             </div>
             <div className='right'>
-                <div className="search">
+                <div className="search" >
                     <input 
                         type="text" 
                         placeholder='Another location'
@@ -63,37 +110,40 @@ console.log(infos.main)
                         onChange={(e)=>setInput(e.target.value)}
                         required
                     />
-                    <button type='submit' onClick={handleSubmit}><div><CiSearch/></div></button>
+                    <button className='buttonSearch' onClick={handleSubmit}><div><CiSearch/></div></button>
+                </div>
+                <div className="cities">
+                    <div className="city"><button onClick={() => setLocation("rabat")}>Rabat</button></div>
+                    <div className="city"><button onClick={() => setLocation("paris")}>Paris</button></div>
+                    <div className="city"><button onClick={() => setLocation("madrid")}>Madrid</button></div>
+                    <div className="city"><button onClick={() => setLocation("amsterdam")}>Amsterdam</button></div>
                 </div>
                 <div className="infos">
                     <h2>Weather Details</h2>
                     <div >
                         <div className="info">
                             <span>Cloudy    :</span>
-                            <span>12%</span>
+                            <span>{infos.clouds.all}%</span>
                         </div>
                         <div className="info">
                             <span>Humidity  :</span>
-                            <span>78%</span>
+                            <span>{infos.main.humidity}%</span>
                         </div>
                         <div className="info">
                             <span>Wind  :</span>
-                            <span>1Km/H</span>
+                            <span>{infos.wind.speed}Km/H</span>
                         </div>
                         <div className="info"   >
-                            <span>Rain  :</span>
-                            <span>0mm</span>
+                            <span>Pressure  :</span>
+                            <span>{infos.main.pressure}Pa</span>
                         </div>
                     </div>
                 </div>
             </div>
+            </>
+            ):('LOADING...')
+    
+}
         </div>
     )
 };
-
-
-
-
-
-
-
